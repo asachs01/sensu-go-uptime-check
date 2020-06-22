e9785d47fdf08d95b703f0beed232b3d8c0b0def
Vladimir Ermakov <vooon341@gmail.com>
2020-06-22 21:48:43 +0300

    main: implement --greater-than

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 //Import the packages we need
 import (
 	"fmt"
+	"io"
 	"os"
 	"time"
-	"io"
 
 	"github.com/sensu/sensu-go/types"
 	"github.com/shirou/gopsutil/host"
@@ -15,7 +15,8 @@ import (
 //Set up some variables. Most notably, warning and critical as time durations
 var (
 	warning, critical time.Duration
-	stdin   *os.File
+	greaterThan       bool
+	stdin             *os.File
 )
 
 //Start our main function
@@ -35,6 +36,12 @@ func configureRootCommand() *cobra.Command {
 		RunE:  run,
 	}
 
+	cmd.Flags().BoolVarP(&greaterThan,
+		"greater-than",
+		"g",
+		false,
+		"This compare uptime > threshold. Default behavior uptime < threshold")
+
 	cmd.Flags().DurationVarP(&warning,
 		"warning",
 		"w",
@@ -46,7 +53,7 @@ func configureRootCommand() *cobra.Command {
 		"c",
 		time.Duration(168*time.Hour),
 		"Critical value in seconds, minutes, or hours default is 1 week (168h)")
-		
+
 	return cmd
 }
 
@@ -60,45 +67,70 @@ func run(cmd *cobra.Command, args []string) error {
 	if stdin == nil {
 		stdin = os.Stdin
 	}
-	
+
 	event := &types.Event{}
-	
+
 	return checkUptime(event)
 }
 
 //Here we start the meat of what we do.
 func checkUptime(event *types.Event) error {
-	
-	//Setting "CheckUptime" as a constant
-	const checkName = "CheckUptime"
-	const metricName = "current_system_uptime"
-	//Setting uptime as the value retrieved from gopsutil
+	// Setting uptime as the value retrieved from gopsutil
 	uptime, err := host.Uptime()
-	
-	//Let's set up some error handling
+
+	// Let's set up some error handling
 	if err != nil {
 		msg := fmt.Sprintf("Failed to determine uptime %s", err.Error())
 		io.WriteString(os.Stdout, msg)
 		os.Exit(3)
 	}
-	
-	//Add a variable for uptimeSecs, which converts uptime to a duration
-	uptimeSecs := time.Duration(uptime)*time.Second
-	
-	//Sets up conditionss for a comparison
+
+	// Add a variable for uptimeSecs, which converts uptime to a duration
+	uptimeSecs := time.Duration(uptime) * time.Second
+
+	if greaterThan {
+		return checkUptimeGreaterThan(uptimeSecs)
+	}
+	return checkUptimeLessThan(uptimeSecs)
+}
+
+func report(res string, uptimeSecs time.Duration) {
+	// Setting "CheckUptime" as a constant
+	const checkName = "CheckUptime"
+	const metricName = "current_system_uptime"
+
+	msg := fmt.Sprintf("%s %s - value = %d | %s=%d\n", checkName, res, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
+	io.WriteString(os.Stdout, msg)
+}
+
+func checkUptimeLessThan(uptimeSecs time.Duration) error {
+	// Sets up conditionss for a comparison
 	if uptimeSecs > critical {
-		msg := fmt.Sprintf("%s CRITICAL - value = %d | %s=%d\n", checkName, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
-		io.WriteString(os.Stdout, msg)
+		report("CRITICAL", uptimeSecs)
 		os.Exit(2)
 	} else if uptimeSecs >= warning && uptimeSecs <= critical {
-		msg := fmt.Sprintf("%s WARNING - value = %d | %s=%d\n", checkName, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
-		io.WriteString(os.Stdout, msg)
+		report("WARNING", uptimeSecs)
 		os.Exit(1)
 	} else {
-		msg := fmt.Sprintf("%s OK - value = %d | %s=%d\n", checkName, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
-		io.WriteString(os.Stdout, msg)
+		report("OK", uptimeSecs)
 		os.Exit(0)
 	}
+
 	return nil
 }
 
+func checkUptimeGreaterThan(uptimeSecs time.Duration) error {
+	// Sets up conditionss for a comparison
+	if uptimeSecs < critical {
+		report("CRITICAL", uptimeSecs)
+		os.Exit(2)
+	} else if uptimeSecs < warning {
+		report("WARNING", uptimeSecs)
+		os.Exit(1)
+	} else {
+		report("OK", uptimeSecs)
+		os.Exit(0)
+	}
+
+	return nil
+}
